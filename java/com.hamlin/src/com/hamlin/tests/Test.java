588713a216352a724492992549b0eb5f73a35c6f
hamlin_gong <pingguolong2011@gmail.com>
2014-03-27 01:15:23 +0800

    Add data test

--- java/com.hamlin/src/com/hamlin/tests/Test.java
+++ java/com.hamlin/src/com/hamlin/tests/Test.java
@@ -12,15 +12,50 @@ public class Test {
      * @param args
      */
     public static void main(String[] args) {
-        SynchronizeTest.myThreadTest();
-        SynchronizeTest.myThread2Test();
-        SynchronizeTest.myThread3Test();
-
-        ObserverTest.test();
-        IteratorTest.test();
-        FactoryMethodTest.test();
-        AbstractFactoryTest.test();
-        Round.test();
+//        SynchronizeTest.myThreadTest();
+//        SynchronizeTest.myThread2Test();
+//        SynchronizeTest.myThread3Test();
+//
+//        ObserverTest.test();
+//        IteratorTest.test();
+//        FactoryMethodTest.test();
+//        AbstractFactoryTest.test();
+//        Round.test();
+    	
+    	int data[] = {1, 4, 5, 7, 9, 12, 15, 46};
+    	exchange(data, 8);
 
     }
+    
+    public static void exchange(int[] data, int length) {
+    	int count = 0;
+     	for (int i = 0; i < length / 2; i++) {
+    		System.out.println(" ---data: " + data[i]);
+    		System.out.println(" ---data % 2: " + (data[i] % 2));
+    		if ((data[i] % 2) == 0) {
+    			do {
+    				System.out.println(" ---data[length -i] % 2---- " + (data[length - i - 1] % 2));
+    				if ((data[length - count - 1] % 2) != 0) {
+        				System.out.println(" ---data[i]---- " + data[i]);
+        				System.out.println(" ---data[length -i]---- " + data[length -i -1]);
+    					int tmp = data[i];
+    					data[i] = data[length -i -1];
+    					data[length -i -1] = tmp;
+    					break;
+    				}
+    				System.out.println(" ---data[i]---- " + data[i]);
+    				System.out.println(" ---data[length -i -1]---- " + data[length -i -1]);
+    				length--; 
+    				count ++;
+    			} while ((data[length - i - 1] % 2) == 0);
+    			int tmp = data[i];
+				data[i] = data[length -i -1];
+				data[length -i -1] = tmp;
+    		}
+    	}
+    	
+    	for (int i = 0; i < 8; i++) {
+    		System.out.println("data: " + data[i]);
+    	}
+    } 
 }
